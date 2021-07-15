dc168dab7c4a0e36fef584b370d6cc8f7188703f
hxie0 <xhy@chicagobooth.edu>
2021-07-15 19:41:24 +0000

    Omnicalc-2 completed!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,68 @@
 class ApplicationController < ActionController::Base
+  def addition_form
+    render("add_templates/addition.html.erb")
+  end
 
+  def addition_results
+    # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first + @second
+    render("add_templates/addition_results.html.erb")
+  end
+
+  def subtraction_form
+    render("add_templates/subtraction.html.erb")
+  end
+
+  def subtraction_results
+    # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first - @second
+    render("add_templates/subtraction_results.html.erb")
+  end
+
+  def multiplication_form
+    render("add_templates/multiplication.html.erb")
+  end
+
+  def multiplication_results
+    # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first * @second
+    render("add_templates/multiplication_results.html.erb")
+  end
+
+  def division_form
+    render("add_templates/division.html.erb")
+  end
+
+  def division_results
+    # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first / @second
+    render("add_templates/division_results.html.erb")
+  end
+
+  def street_to_coords_form
+    render("weather_templates/street_to_coords_form.html.erb")
+  end
+
+  def street_to_coords_results
+    # Parameters: {"user_street_address"=>"Merchandise Mart, Chicago"}
+    @street_address = params.fetch("user_street_address")
+    @maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address + "&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY"
+    @raw_json_string = open(@maps_url).read
+    @parsed_json = JSON.parse(@raw_json_string)
+    @results_array = @parsed_json.fetch("results")
+    @first_result = @results_array.at(0)
+    @geometry_hash = @first_result.fetch("geometry")
+    @location_hash = @geometry_hash.fetch("location")
+    @latitude = @location_hash.fetch("lat")
+    @longitude = @location_hash.fetch("lng")
+    render("weather_templates/street_to_coords_results.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "addition_form" })
+  get("/add", { :controller => "application", :action => "addition_form" })
+  get("/wizard_add", { :controller => "application", :action => "addition_results" })
+  get("/subtract", { :controller => "application", :action => "subtraction_form" })
+  get("/wizard_subtract", { :controller => "application", :action => "subtraction_results" })
+  get("/multiply", { :controller => "application", :action => "multiplication_form" })
+  get("/wizard_multiply", { :controller => "application", :action => "multiplication_results" })
+  get("/divide", { :controller => "application", :action => "division_form" })
+  get("/wizard_divide", { :controller => "application", :action => "division_results" })
+
+  get("/street_to_coords/new", { :controller => "application", :action => "street_to_coords_form" })
+  get("/wizard_street_to_coords", { :controller => "application", :action => "street_to_coords_results" })
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
